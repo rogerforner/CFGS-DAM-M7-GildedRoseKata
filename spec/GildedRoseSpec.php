c17d233101b99910667057a08034d8b49ae8cca4
Roger Forner Fabre <sir.rogerforner@gmail.com>
2016-11-27 22:26:40 +0100

    setup i test de acacha/Gilded-Rose

--- spec/GildedRoseSpec.php
+++ spec/GildedRoseSpec.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace spec;
+namespace spec\App;
 
 
 use App\GildedRose;
@@ -11,6 +11,15 @@ class GildedRoseSpec extends ObjectBehavior
 {
     function it_is_initializable()
     {
+        $this->beConstructedWith('normal',10,5);
         $this->shouldHaveType(GildedRose::class);
+    }
+
+    function it_updates_normal_items_before_sell_date()
+    {
+        $this->beConstructedWith('normal',10,5);
+        $this->tick();
+        $this->quality->shouldBe(9);
+        $this->sellIn->shouldBe(4);
     }
 }
