b2cd413fc5a47fe34951071d33e21be7f0d96386
Roger Forner Fabre <sir.rogerforner@gmail.com>
2016-11-29 13:56:33 +0100

    refactorització, millorant codi

--- src/GildedRose.php
+++ src/GildedRose.php
@@ -19,58 +19,68 @@ class GildedRose
 
     public function tick()
     {
-        if ($this->name == 'Aged Brie'){
+        switch ($this->name){
 
-            if ($this->quality < 50)
-                $this->quality = $this->quality + 1;
-
-            $this->sellIn = $this->sellIn - 1;
-
-            if ($this->sellIn < 0) {
-                if ($this->quality < 50)
-                    $this->quality = $this->quality + 1;
-            }
-        } elseif ($this->name == 'Backstage passes to a TAFKAL80ETC concert'){
-
-            if ($this->quality < 50)
-                $this->quality = $this->quality + 1;
-            if ($this->sellIn < 11)
+            case 'Aged Brie':
                 if ($this->quality < 50)
                     $this->quality = $this->quality + 1;
 
-            if ($this->sellIn < 6)
+                $this->sellIn = $this->sellIn - 1;
+
+                if ($this->sellIn < 0) {
+                    if ($this->quality < 50)
+                        $this->quality = $this->quality + 1;
+                }
+                break;
+
+            case 'Backstage passes to a TAFKAL80ETC concert':
+
                 if ($this->quality < 50)
                     $this->quality = $this->quality + 1;
+                if ($this->sellIn < 11)
+                    if ($this->quality < 50)
+                        $this->quality = $this->quality + 1;
 
-            $this->sellIn = $this->sellIn - 1;
+                if ($this->sellIn < 6)
+                    if ($this->quality < 50)
+                        $this->quality = $this->quality + 1;
 
-            if ($this->sellIn < 0) {
-                $this->quality = $this->quality - $this->quality;
-            }
-        } elseif ($this->name == 'Sulfuras, Hand of Ragnaros'){
+                $this->sellIn = $this->sellIn - 1;
 
-        } elseif ($this->name == 'Conjured Mana Cake'){
-            if ($this->quality > 0)
-                $this->quality = $this->quality - 2;
+                if ($this->sellIn < 0) {
+                    $this->quality = $this->quality - $this->quality;
+                }
+                break;
 
-            $this->sellIn = $this->sellIn - 1;
+            case 'Sulfuras, Hand of Ragnaros':
+                break;
 
-            if ($this->sellIn < 0) {
+            case 'Conjured Mana Cake':
                 if ($this->quality > 0)
                     $this->quality = $this->quality - 2;
-            }
-        }
-        else {
-            if ($this->quality > 0)
-                $this->quality = $this->quality - 1;
 
-            $this->sellIn = $this->sellIn - 1;
+                $this->sellIn = $this->sellIn - 1;
 
-            if ($this->sellIn < 0) {
+                if ($this->sellIn < 0) {
+                    if ($this->quality > 0)
+                        $this->quality = $this->quality - 2;
+                }
+                break;
+
+            default:
                 if ($this->quality > 0)
                     $this->quality = $this->quality - 1;
-            }
+
+                $this->sellIn = $this->sellIn - 1;
+
+                if ($this->sellIn < 0) {
+                    if ($this->quality > 0)
+                        $this->quality = $this->quality - 1;
+                }
+                break;
+
         }
+
 
     }
 }
